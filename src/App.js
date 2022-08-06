7a84cd96ab153104024af2ce51e089006f89b6f9
Danilo Martins <dansm86@outlook.com>
2022-08-06 02:14:18 -0300

    req6-criado logica para renderizar perguntas
    Estou usando perguntas mockadas, não fiz a lógica
    requisição API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
-// import logo from './trivia.png';
+import logo from './trivia.png';
 import './App.css';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <div className="App">
+      <header>
+        <img src={ logo } className="App-logo" alt="logo" />
+      </header>
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route path="/game" component={ Game } />
       </Switch>
     </div>
   );
---
+++ src/pages/Game.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+import questionsResponse from '../utils/constants';
+
+class Game extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      activeIndex: 0,
+    };
+  }
+
+  getSortedAnswers(item) {
+    const sortFactor = 0.5;
+    const { correct_answer: correctAnswer, incorrect_answers: incorrectAnswers } = item;
+    const answersList = [correctAnswer, ...incorrectAnswers];
+    const sortedAnswersList = answersList.sort(() => Math.random() - sortFactor);
+    return sortedAnswersList;
+  }
+
+  showNextQuestion = () => {
+    this.setState((PrevState) => ({
+      activeIndex: PrevState.activeIndex + 1,
+    }));
+  }
+
+  renderBtn(bool, str, number) {
+    if (bool) {
+      return (
+        <button key={ number } type="button" data-testid="correct-answer">{ str }</button>
+      );
+    }
+    return (
+      <button
+        key={ number }
+        type="button"
+        data-testid={ `wrong-answer-${number}` }
+      >
+        { str }
+      </button>
+    );
+  }
+
+  renderQuestions = () => {
+    const { activeIndex } = this.state;
+    const { results } = questionsResponse;
+    return results.map((item, index) => {
+      const isActive = activeIndex === index;
+      return (
+        <section
+          key={ index }
+          style={ isActive ? null : { display: 'none' } }
+        >
+          <h2>{ item.category }</h2>
+          <h3>{ item.question }</h3>
+          <div data-testid="answer-options">
+            {
+              this.getSortedAnswers(item).map((answer, indexAnsw) => {
+                if (answer === item.correct_answer) {
+                  return this.renderBtn(true, answer, indexAnsw);
+                }
+                return this.renderBtn(false, answer, indexAnsw);
+              })
+            }
+          </div>
+        </section>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.renderQuestions()
+        }
+      </div>
+    );
+  }
+}
+
+export default Game;

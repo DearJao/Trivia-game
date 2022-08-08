7613c709f0af22f08f94635a0ff6121b3a1e3159
Dear_Jao <jvvr2013@gmail.com>
2022-08-08 18:03:00 -0300

    req13

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const minimumScore = 3;
+    const { score } = this.props;
+
     return (
       <div>
         <Header />
+        <p data-testid="feedback-text">
+          { score < minimumScore ? 'Could be better...' : 'Well Done!' }
+        </p>
       </div>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  score: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = (store) => ({
+  score: store.player.score,
+});
+
+export default connect(mapStateToProps)(Feedback);
